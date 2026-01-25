d67332193c88520f3d0aa5196a31952bfbc26c89
David <davidgomesbrigido@gmail.com>
2026-01-25 11:38:06 -0300

    feat: create and use `NotFoundError` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -58,6 +58,27 @@ export class ValidationError extends Error {
   }
 }
 
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Was not possible to find this resource in the system.", {
+      cause,
+    });
+    this.name = "NotFoundError";
+    this.action =
+      action || "Check if the parameters sent in the query are correct.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("Method forbidden for this endpoint.");
