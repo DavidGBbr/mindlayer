359f7bdaeeb7398fff1946787c8ad381fe920f44
David <davidgomesbrigido@gmail.com>
2026-01-27 21:03:18 -0300

    feat: add suport for `PATCH` on `/api/v1/users/[username]`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -85,7 +85,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "The email address provided is already in use.",
-        action: "Use a different email address to register.",
+        action: "Use a different email address for this operation.",
         status_code: 400,
       });
     });
@@ -124,7 +124,7 @@ describe("POST /api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "The username provided is already in use.",
-        action: "Use a different username to register.",
+        action: "Use a different username for this operation.",
         status_code: 400,
       });
     });
